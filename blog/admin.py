828b88d163f9e6725a827c9b58e8caf2895c1071
pokotsun <gorigolila@gmail.com>
2018-04-08 20:25:12 +0900

    REST FRAMEWORKを作成

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import User, Entry
+
+# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from blog.urls import router as blog_router
 
 from django.conf.urls import include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+
+    # blog.urlsをincludeする
+    url(r'^api/', include(blog_router.urls))
 ]

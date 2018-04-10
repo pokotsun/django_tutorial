a3123afa20f223b363133a8d284083473c78ef59
pokotsun <gorigolila@gmail.com>
2018-04-10 23:51:54 +0900

    Serializerのカスタム

--- blog/serializer.py
+++ blog/serializer.py
@@ -8,8 +8,22 @@ from .models import User, Entry
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'mail')
+        # APIとして出力したいfieldのタプル
+        fields = ('id', 'name', 'mail',)
 
+class CustomUserSerializer(UserSerializer):
+    # ここでこのように宣言することでモデル外の変数をcolumnの一部のように扱える
+    value = serializers.SerializerMethodField()
+     
+    class Meta:
+        model = User
+        # APIとして出力したいfieldのタプル
+        fields = ('id', 'name', 'mail', 'value')
+
+    # このget_methodで取得が可能となる
+    def get_value(self, obj):
+        return obj.name
+    
 
 class EntrySerializer(serializers.ModelSerializer):
     # authorのserializerを上書きすることっで、入れ子にできる
--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,16 @@ import django_filters
 from rest_framework import viewsets, filters
 
 from .models import User, Entry
-from .serializer import UserSerializer, EntrySerializer
+from .serializer import UserSerializer, EntrySerializer, CustomUserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
+    # ここでfilterをかけてやれば大丈夫
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(name="pokotsun")
+    serializer_class = CustomUserSerializer
 
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
